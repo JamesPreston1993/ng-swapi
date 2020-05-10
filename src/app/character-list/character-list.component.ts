b6e64027c3070bc7a60379b0508d8c46d768510e
James Preston <jamescpreston93@gmail.com>
2020-05-10 16:52:30 +0100

    Display list of characters

--- src/app/character-list/character-list.component.ts
+++ src/app/character-list/character-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { CharacterService } from '../services/character.service';
+import { Character } from '../models/Character';
 
 @Component({
   selector: 'app-character-list',
   templateUrl: './character-list.component.html',
-  styleUrls: ['./character-list.component.css']
+  styleUrls: ['./character-list.component.css'],
 })
 export class CharacterListComponent implements OnInit {
-
-  constructor() { }
+  characters: Character[];
+  constructor(private _characterService: CharacterService) {}
 
   ngOnInit(): void {
+    this._characterService
+      .getCharacters()
+      .subscribe((response) => (this.characters = response));
   }
-
 }
