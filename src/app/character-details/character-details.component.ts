0555a5cd64bf94fe10bd9031c42100a3630278d5
James Preston <jamescpreston93@gmail.com>
2020-05-11 19:59:18 +0100

    Get character details in component

--- src/app/character-details/character-details.component.ts
+++ src/app/character-details/character-details.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { CharacterService } from '../services/character.service';
+import { ActivatedRoute } from '@angular/router';
+import { Character } from '../models/Character';
 
 @Component({
   selector: 'app-character-details',
   templateUrl: './character-details.component.html',
-  styleUrls: ['./character-details.component.css']
+  styleUrls: ['./character-details.component.css'],
 })
 export class CharacterDetailsComponent implements OnInit {
+  character: Character;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private _characterService: CharacterService
+  ) {}
 
   ngOnInit(): void {
-  }
+    const characterId = +this.route.snapshot.paramMap.get('characterId');
 
+    this._characterService
+      .getCharacter(characterId)
+      .subscribe((response) => (this.character = response));
+  }
 }
