ce9003cee452a506652ac133474e0b717913ceff
James Preston <jamescpreston93@gmail.com>
2020-05-12 20:00:23 +0100

    Update tests to use jasmine spys

--- src/app/services/character.service.spec.ts
+++ src/app/services/character.service.spec.ts
@@ -1,43 +1,81 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CharacterService } from './character.service';
+import { HttpClient } from '@angular/common/http';
+import { Character } from '../models/Character';
+import { of } from 'rxjs';
 
 describe('CharacterService', () => {
   describe('getCharacters', () => {
     let service: CharacterService;
+    let mockHttpClient: jasmine.SpyObj<HttpClient>;
 
     beforeEach(() => {
-      TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
-      service = TestBed.inject(CharacterService);
+      mockHttpClient = jasmine.createSpyObj(['get']);
+      service = new CharacterService(mockHttpClient);
+
+      let characters: Character[] = [
+        {
+          name: 'FirstName LastName',
+          birth_year: '10 BBY',
+          hair_color: 'Black',
+          skin_color: 'Fair',
+          eye_color: 'Blue',
+          gender: 'Male',
+          height: 180,
+          mass: 85,
+        },
+      ];
+
+      mockHttpClient.get.and.returnValue(
+        of({
+          count: 1,
+          previous: 'previousUrl',
+          next: 'nextUrl',
+          results: characters,
+        })
+      );
     });
 
-    it('should return list of characters', () => {
-      // arrange
-
+    it('should call http client get', () => {
       // act
-      const characters = service.getCharacters(1);
+      service.getCharacters(1);
 
       // assert
-      expect(characters).not.toBeNull;
+      expect(mockHttpClient.get).toHaveBeenCalledWith(
+        'https://swapi.dev/api/people?page=1'
+      );
     });
   });
 
   describe('getCharacter', () => {
     let service: CharacterService;
+    let mockHttpClient: jasmine.SpyObj<HttpClient>;
 
     beforeEach(() => {
-      TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
-      service = TestBed.inject(CharacterService);
+      mockHttpClient = jasmine.createSpyObj(['get']);
+      service = new CharacterService(mockHttpClient);
+
+      let character: Character = {
+        name: 'FirstName LastName',
+        birth_year: '10 BBY',
+        hair_color: 'Black',
+        skin_color: 'Fair',
+        eye_color: 'Blue',
+        gender: 'Male',
+        height: 180,
+        mass: 85,
+      };
+
+      mockHttpClient.get.and.returnValue(of(character));
     });
 
-    it('should return character', () => {
-      // arrange
-
+    it('should call http client get', () => {
       // act
-      const character = service.getCharacter(1);
+      service.getCharacter(1);
 
       // assert
-      expect(character).not.toBeNull;
+      expect(mockHttpClient.get).toHaveBeenCalledWith(
+        'https://swapi.dev/api/people/1'
+      );
     });
   });
 });
