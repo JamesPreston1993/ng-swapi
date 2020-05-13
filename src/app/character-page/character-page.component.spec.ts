80c5a43f0c4ae764cad041ccf59655a9d625c33e
James Preston <jamescpreston93@gmail.com>
2020-05-13 09:12:23 +0100

    Test the loading spinner appears

--- src/app/character-page/character-page.component.spec.ts
+++ src/app/character-page/character-page.component.spec.ts
@@ -1,24 +1,53 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { of } from 'rxjs';
 
 import { CharacterPageComponent } from './character-page.component';
+import { CharacterService } from '../services/character.service';
+import { Character } from '../models/Character';
 
 describe('CharacterPageComponent', () => {
    let component: CharacterPageComponent;
    let fixture: ComponentFixture<CharacterPageComponent>;
-
-   beforeEach(async(() => {
-      TestBed.configureTestingModule({
-         declarations: [CharacterPageComponent]
-      }).compileComponents();
-   }));
+   let character: Character;
 
    beforeEach(() => {
+      character = {
+         name: 'FirstName LastName',
+         birth_year: '10 BBY',
+         hair_color: 'Black',
+         skin_color: 'Fair',
+         eye_color: 'Blue',
+         gender: 'Male',
+         height: 180,
+         mass: 85
+      };
+
+      TestBed.configureTestingModule({
+         declarations: [CharacterPageComponent],
+         providers: [
+            {
+               provide: ActivatedRoute,
+               useValue: { snapshot: { paramMap: convertToParamMap({ characterId: 1 }) } }
+            },
+            {
+               provide: CharacterService,
+               useValue: { getCharacter: () => of(character) }
+            }
+         ]
+      });
+
       fixture = TestBed.createComponent(CharacterPageComponent);
       component = fixture.componentInstance;
       fixture.detectChanges();
    });
 
-   it('should create', () => {
-      expect(component).toBeTruthy();
+   it('should display loading spinner before calling the service', () => {
+      component.isLoading = true;
+      fixture.detectChanges();
+
+      // assert
+      const element = fixture.nativeElement.querySelector('.spinner-border');
+      expect(element).not.toBeNull();
    });
 });
